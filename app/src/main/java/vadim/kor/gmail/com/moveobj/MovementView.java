2c0a44eecc41f740735461ea657f5dbee6210912
Vadim Korepanov <krpnv@mail.ru>
2018-04-21 18:43:02 +0300

    Added rotation

--- app/src/main/java/vadim/kor/gmail/com/moveobj/MovementView.java
+++ app/src/main/java/vadim/kor/gmail/com/moveobj/MovementView.java
@@ -1,14 +1,15 @@
 package vadim.kor.gmail.com.moveobj;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.ImageView;
 
 public class MovementView extends SurfaceView implements SurfaceHolder.Callback {
     private int xPos;
@@ -24,6 +25,9 @@ public class MovementView extends SurfaceView implements SurfaceHolder.Callback 
     private Paint circlePaint;
 
     private Bitmap ball;
+    private int ballWidth;
+    private int ballHeight;
+    private int currentAngle;
 
     UpdateThread updateThread;
 
@@ -35,6 +39,9 @@ public class MovementView extends SurfaceView implements SurfaceHolder.Callback 
         getHolder().addCallback(this);
 
         ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+        ballWidth = ball.getWidth();
+        ballHeight = ball.getHeight();
+        currentAngle = 0;
         circleRadius = 40;
         circlePaint = new Paint();
         circlePaint.setColor(Color.BLUE);
@@ -46,13 +53,17 @@ public class MovementView extends SurfaceView implements SurfaceHolder.Callback 
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.WHITE);
         //canvas.drawCircle(xPos, yPos, circleRadius, circlePaint);
-        canvas.drawBitmap(ball, xPos, yPos, null);
+        @SuppressLint("DrawAllocation")
+        Matrix mxTransform = new Matrix();
+        mxTransform.preTranslate(xPos, yPos);
+        mxTransform.preRotate(currentAngle, ballWidth / 2, ballHeight / 2);
+        canvas.drawBitmap(ball, mxTransform, null);
     }
 
     public void updatePhysics() {
         xPos += xVel;
         yPos += yVel;
-
+        currentAngle += 2;
         if (yPos - circleRadius < 0 || yPos + circleRadius > height) {
             if (yPos - circleRadius < 0) {
                 yPos = circleRadius;
